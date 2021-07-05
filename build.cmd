9e17dab7ae836dd12a5d468b7fbe06e5ded228a8
innc11 <aprilforests@qq.com>
2021-07-05 11:39:17 +0800

    ci: 优化本地构建脚本

--- build.cmd
+++ build.cmd
@@ -1,24 +1,4 @@
 @echo off
 
 python ci\generate_version_file.py
-
-python ci\version.py both > temp.txt
-set /p filename=< temp.txt
-
-python ci\version.py version > temp.txt
-set /p version_text=< temp.txt
-
-del temp.txt
-
-del /f /s /q dist\%filename%.exe
-
-echo ----------------Build for %filename%----------------
-
-pyinstaller --noconfirm --version-file version-file.txt -i icon.ico -c -F -n %filename% main.py
-
-echo ----------------Clean up----------------
-
-del /f /s /q version-file.txt
-del /f /s /q %filename%.spec
-
-echo ----------------Build %filename% finished!----------------
+pyinstaller --noconfirm --version-file version-file.txt -i icon.ico -c -F -n Tool main.py
